3d50249685096fdab316602ae00a23baa500e6c4
CodyHenrichsen-CTEC <codyhenrichsen@Cody-Henrichsens-MacBook-Pro-2.local>
2014-10-05 18:57:18 -0600

    updated gui

--- src/chatbox/controller/ChatboxAppController.java
+++ src/chatbox/controller/ChatboxAppController.java
@@ -2,8 +2,24 @@ package chatbox.controller;
 
 import javax.swing.JOptionPane;
 
+import chatbox.view.ChatboxView;
+import chatbox.model.ChatBox;
+
 public class ChatboxAppController
 {
+    	private ChatboxView appView;
+    	private ChatBox grandpaBot;
+    	private String startMessage;
+    	private String endMessage;
+    	
+    	public ChatboxAppController()
+    	{
+    	    appView = new ChatboxView(this);
+    	    grandpaBot = new ChatBox("Grandpa");
+    	    startMessage = "Lets Begin";
+    	    endMessage = "All Done!";
+    		    
+    	}
 	public void start()
 	{
 		
